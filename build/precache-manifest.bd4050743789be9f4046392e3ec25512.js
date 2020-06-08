85a4ff1eea435014c811ba38d5cbe756e2766cc4
muhammadisa <muhammad.isa@gudangada.com>
2020-06-08 21:56:13 +0700

    Build on Heroku

---
+++ build/precache-manifest.bd4050743789be9f4046392e3ec25512.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ad2747a1e4ceef1e5d43e3a4ebd1ee3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "776bda5047ec79d50060",
+    "url": "/static/css/main.45b2bdba.chunk.css"
+  },
+  {
+    "revision": "4f01c89cdab531f1e09b",
+    "url": "/static/js/2.28ce6c3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.28ce6c3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "776bda5047ec79d50060",
+    "url": "/static/js/main.09b800cd.chunk.js"
+  },
+  {
+    "revision": "89441414cf857fd4ade8",
+    "url": "/static/js/runtime-main.32f4679e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -9,23 +9,29 @@ function between(x, min, max) {
 function determineCaption(condition) {
   const nightCaption = [
     "Jangan lupa baca doa tidurnya",
-    "Mimpi indah yah yulia",
+    "Jangan sampe salah bantal, nanti pegel pegel",
+    "Jangan begadang terus yah",
+    "Charge hapenya jangan lupa, klo besok mau pergi",
+    "Mimpi indah yah",
   ]
 
   const morningCaption = [
     "Jangan lupa bersyukur dan jangan lupa sarapan :D",
-    "E-Learning lho jangan lupa, jangan lupa sarapan ocew",
+    "E-Learning lho jangan lupa, sarapan juga ocew",
+    "Hati hati pas berangkat ngampus ya",
     "Semangat untuk hari ini, jangan lupa sarapan ya",
   ]
 
   const afternoonCaption = [
     "Kalau pulang ngampus hati hati ya",
     "Selalu hati hati di jalan yahh",
-    "Selalu berdoa saat jalan balik ocew"
+    "Selalu berdoa saat jalan balik ocew",
+    "Di check check barang bawaan nya, jangan sampe ketinggalan di kampus"
   ]
 
   const noonCaption = [
     "Pastinya masih semangat dong :D",
+    "Ganbatteeeeeee",
     "Semangat ya, eh ya jangan lupa makan, udah siang nih",
   ]
 
@@ -47,94 +53,81 @@ function determineCaption(condition) {
   }
 }
 
-function determineBackgroundColor(condition) {
-  switch (condition) {
-    case "NIGHT":
-      return ["#34495e", "#ffffff"]
-    case "MORNING":
-      return ["#ecf0f1", "#000000"]
-    case "NOON":
-      return ["#ecf0f1", "#000000"]
-    case "AFTERNOON":
-      return ["#ecf0f1", "#000000"]
-    default:
-      return ["#ecf0f1", "#000000"]
-  }
-}
-
 function determineGreeting() {
   var today = new Date()
   var curHr = today.getHours()
-  var curMt = today.getHours()
 
   if (between(curHr, 0, 1)) {
-    return "NIGHT,Selamat Pagi :)" // Night
+    return "NIGHT,Selamat Malam" // Night
   } else if (between(curHr, 1, 2)) {
-    return "NIGHT,Selamat Malam :)" // Night
+    return "NIGHT,Selamat Malam" // Night
   } else if (between(curHr, 2, 3)) {
-    return "NIGHT,Selamat Malam :)" // Night
+    return "NIGHT,Selamat Malam" // Night
   } else if (between(curHr, 3, 4)) {
-    return "NIGHT,Selamat Malam :)" // Night
+    return "NIGHT,Selamat Malam" // Night
   } else if (between(curHr, 4, 5)) {
-    return "MORNING,Selamat Pagi :)" // Morning
+    return "MORNING,Selamat Pagi" // Morning
   } else if (between(curHr, 5, 6)) {
-    return "MORNING,Selamat Pagi :)" // Morning
+    return "MORNING,Selamat Pagi" // Morning
   } else if (between(curHr, 6, 7)) {
-    return "MORNING,Selamat Pagi :)" // Morning
+    return "MORNING,Selamat Pagi" // Morning
   } else if (between(curHr, 7, 8)) {
-    return "MORNING,Selamat Pagi :)" // Morning
+    return "MORNING,Selamat Pagi" // Morning
   } else if (between(curHr, 8, 9)) {
-    return "MORNING,Selamat Pagi :)" // Morning
+    return "MORNING,Selamat Pagi" // Morning
   } else if (between(curHr, 9, 10)) {
-    return "MORNING,Selamat Pagi :)" // Morning
+    return "MORNING,Selamat Pagi" // Morning
   } else if (between(curHr, 10, 11)) {
-    return "NOON,Selamat Siang :)" // Noon
+    return "NOON,Selamat Siang" // Noon
   } else if (between(curHr, 11, 12)) {
-    return "NOON,Selamat Siang :)" // Noon
+    return "NOON,Selamat Siang" // Noon
   } else if (between(curHr, 12, 13)) {
-    return "NOON,Selamat Siang :)" // Noon
+    return "NOON,Selamat Siang" // Noon
   } else if (between(curHr, 13, 14)) {
-    return "NOON,Selamat Siang :)" // Noon
+    return "NOON,Selamat Siang" // Noon
   } else if (between(curHr, 14, 15)) {
-    return "NOON,Selamat Siang :)" // Noon
+    return "NOON,Selamat Siang" // Noon
   } else if (between(curHr, 15, 16)) {
-    return "AFTERNOON,Selamat Sore :)" // Afternoon
+    return "AFTERNOON,Selamat Sore" // Afternoon
   } else if (between(curHr, 16, 17)) {
-    return "AFTERNOON,Selamat Sore :)" // Afternoon
+    return "AFTERNOON,Selamat Sore" // Afternoon
   } else if (between(curHr, 17, 18)) {
-    return "AFTERNOON,Selamat Sore :)" // Afternoon
+    return "AFTERNOON,Selamat Sore" // Afternoon
   } else if (between(curHr, 18, 19)) {
-    return "NIGHT,Selamat Malam :)" // Night
+    return "NIGHT,Selamat Malam" // Night
   } else if (between(curHr, 19, 20)) {
-    return "NIGHT,Selamat Malam :)" // Night
+    return "NIGHT,Selamat Malam" // Night
   } else if (between(curHr, 20, 21)) {
-    return "NIGHT,Selamat Malam :)" // Night
+    return "NIGHT,Selamat Malam" // Night
   } else if (between(curHr, 21, 22)) {
-    return "NIGHT,Selamat Malam :)" // Night
+    return "NIGHT,Selamat Malam" // Night
   } else if (between(curHr, 22, 23)) {
-    return "NIGHT,Selamat Malam :)" // Night
+    return "NIGHT,Selamat Malam" // Night
   } else if (between(curHr, 23, 24)) {
-    return "NIGHT,Selamat Malam :)" // Night
+    return "NIGHT,Selamat Malam" // Night
   }
 }
-
-let color = determineBackgroundColor(determineGreeting().split(",")[0])
 
 function App() {
   return (
     <div className="App">
       <header className="App-header" style={{
-        color: color[1],
-        backgroundColor: color[0]
+        color: "#000000",
+        backgroundColor: "#ecf0f1"
       }}>
-        <p style={{
-          fontSize: "64px"
-        }}>
-          {determineGreeting().split(",")[1]}
-        </p>
-        <h6>
-          {determineCaption(determineGreeting().split(",")[0])}
-        </h6>
+        <div>
+          <h1 style={{
+            fontSize: "64px",
+            fontWeight: "bold"
+          }}>
+            {determineGreeting().split(",")[1]}
+          </h1>
+          <p style={{
+            fontWeight: "normal"
+          }}>
+            {determineCaption(determineGreeting().split(",")[0])}
+          </p>
+        </div>
       </header>
     </div>
   );
